01cc7e48d87cb74eaceef0bd747039e36b8488e7
Dominik Hrinkino <admin@hrinkino.com>
2025-08-19 14:04:06 +0200

    Added check to not append existing configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Check if configuration is already deployed
+if grep -q "push" /etc/nginx/nginx.conf; then
+    stunnel /etc/stunnel/stunnel.conf
+    exec nginx -g 'daemon off;'
+    exit 0
+  fi
+
 # FOREACH FOR TWITCH/KICK
 for TW_ENDPOINT in $TWITCH_ENDPOINTS; do
     echo "                        push rtmp://ingest.global-contribute.live-video.net/app/$TW_ENDPOINT;" >> /etc/nginx/nginx.conf
